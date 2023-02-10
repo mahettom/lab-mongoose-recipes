ff9e02ce3a38959f6d202000652df8f6dee6d21c
TomMahe <mahettom@gmail.com>
2023-02-10 17:57:49 +0100

    done

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
+const Recipe = require('./models/Recipe.model');
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
@@ -10,14 +10,60 @@ const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
+
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  // Iteration 2 ———  Create a recipe and print the title 
+  .then(async () => {
+    const recipe1 = await Recipe.create({
+      title: "eggs",
+      level: "Easy Peasy",
+      ingredients: ["eggs"],
+      cuisine: "small",
+      dishType: "other",
+      image: "",
+      duration: 1,
+      creator: "the chicken or the egg, who knows",
+      created: '0000-12-09',
+    })
+    console.log(recipe1.title);
   })
+
+  // Iteration 3 ——— Insert multiple recipes and print title
+  .then(async () => {
+
+    const dataRecipe = await Recipe.insertMany(data)
+
+    dataRecipe.forEach(one => {
+      console.log(`${one.title}`);
+    });
+  })
+
+  // Iteration 4 ——— update duration to 100
+  .then(async () => {
+    const update = await Recipe.findOneAndUpdate(
+      { title: 'Rigatoni alla Genovese' },
+      { duration: 100 },
+      { new: true },
+    );
+
+    console.log(`The ${update.title} has been update`);
+  })
+
+  // Iteration 5 ——— Remove a recipe
+  .then(async () => {
+    const remove = await Recipe.deleteOne({ title: 'Carrot Cake' })
+    console.log(`woops, no more Carrot Cake`);
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+
+  .finally(() => {
+    mongoose.connection.close()
+  })
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,43 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+
+  title: {
+    type: Schema.Types.String,
+    required: true,
+    max: 1,
+  },
+
+  level: {
+    type: Schema.Types.String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+
+  ingredients: [String],
+
+  cuisine: Schema.Types.String,
+
+  dishType: {
+    type: Schema.Types.String,
+    enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"],
+  },
+
+  image: {
+    type: Schema.Types.String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+
+  duration: {
+    type: Schema.Types.Number,
+    min: 0,
+  },
+
+  creator: Schema.Types.String,
+
+  created: {
+    type: Schema.Types.Date,
+    default: Date.now
+  },
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
